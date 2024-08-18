d0ae60da3fc75a87abc4394a24e83d7f8e3e7660
MaksimVolkov <skazimko@yandex.ru>
2024-08-18 11:37:34 +0300

    update commerce.py and tsts for it

--- src/commerce.py
+++ src/commerce.py
@@ -11,6 +11,26 @@ class Product:
         self.price = price
         self.quantity = quantity
 
+    @classmethod
+    def new_product(cls, new_product: dict):
+        """Взвращает созданный объект класса Product из параметров товара в словаре"""
+        name = new_product["name"]
+        description = new_product["description"]
+        price = new_product["price"]
+        quantity = new_product["quantity"]
+        return cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        if value <= 0:
+            print("Цена не должна быть нулевая или орицательная")
+        else:
+            self.__price = value
+
 
 class Category:
     """ Информация о категориях"""
@@ -24,7 +44,19 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         Category.category_count += 1
         Category.product_count += len(products)
+
+
+    @property
+    def products(self):
+        products_str = ""
+        for product in self.__products:
+            products_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return products_str
+
+    def add_product(self, product) -> None:
+        self.__products.append(product)
+        Category.product_count += 1
