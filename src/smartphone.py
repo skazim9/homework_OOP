63af9be6097d5c46f2839cfddc226cab78d469ad
MaksimVolkov <skazimko@yandex.ru>
2024-08-30 14:52:50 +0300

    add class smartphone

---
+++ src/smartphone.py
@@ -0,0 +1,22 @@
+from src.commerce import Product
+
+
+class Smartphone(Product):
+    """Класс для категории <<Смартфоны>>."""
+
+    def __init__(
+        self,
+        name: str,
+        description: str,
+        price: float,
+        quantity: int,
+        efficiency: float,
+        model: str,
+        memory: int,
+        color: str,
+    ):
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
