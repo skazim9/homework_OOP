584134a64955ad159c7617cef65b4131e333526b
MaksimVolkov <skazimko@yandex.ru>
2024-08-30 15:12:50 +0300

    create tests for classes smartphone and lawngrass

---
+++ tests/test_smartphone.py
@@ -0,0 +1,45 @@
+import pytest
+
+from src.smartphone import Smartphone
+
+@pytest.fixture
+def smartphone1():
+    return Smartphone(
+        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5, "S23 Ultra", 256, "Серый"
+    )
+
+
+@pytest.fixture
+def smartphone2():
+    return Smartphone("Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2, "15", 512, "Gray space")
+
+
+def test_smartphone(smartphone1, smartphone2):
+    assert smartphone1.name == "Samsung Galaxy S23 Ultra"
+    assert smartphone1.description == "256GB, Серый цвет, 200MP камера"
+    assert smartphone1.price == 180000.0
+    assert smartphone1.quantity == 5
+    assert smartphone1.efficiency == 95.5
+    assert smartphone1.model == "S23 Ultra"
+    assert smartphone1.memory == 256
+    assert smartphone1.color == "Серый"
+
+    assert smartphone2.name == "Iphone 15"
+    assert smartphone2.description == "512GB, Gray space"
+    assert smartphone2.price == 210000.0
+    assert smartphone2.quantity == 8
+    assert smartphone2.efficiency == 98.2
+    assert smartphone2.model == "15"
+    assert smartphone2.memory == 512
+    assert smartphone2.color == "Gray space"
+
+
+def test_add_smartphone(smartphone1, smartphone2):
+
+    assert smartphone1 + smartphone2 == '2580000.0 руб.'
+
+
+def test_add_smartphone_error(smartphone1):
+
+    with pytest.raises(TypeError):
+        smartphone1 + 1
